5819e62182357438959ee7bde844f5842cc632a0
Roberto J Rojas <robertojrojas@gmail.com>
2017-12-15 14:57:37 -0500

    adds dhcp set + naming

--- main.cpp
+++ main.cpp
@@ -4,6 +4,11 @@
 #include <aws/ec2/model/CreateVpcRequest.h>
 #include <aws/ec2/model/CreateTagsRequest.h>
 #include <aws/ec2/model/DescribeVpcsRequest.h>
+#include <aws/ec2/model/DhcpConfiguration.h>
+#include <aws/ec2/model/DhcpOptions.h>
+#include <aws/ec2/model/CreateDhcpOptionsRequest.h>
+#include <aws/ec2/model/AssociateDhcpOptionsRequest.h>
+#include <aws/ec2/model/NewDhcpConfiguration.h>
 #include <iostream>
 #include <unordered_map>
 
@@ -11,6 +16,7 @@
 #define EMPTY ""
 #define NO_ERROR EMPTY
 #define VPC_NAME_PREFIX  "oneliner"
+#define VPC_NAME "oneliner-vpc" // TODO: Figure out concat strings
 
 const Aws::String nameResource(const Aws::EC2::EC2Client &ec2, const Aws::String& tagName, const Aws::String& resourceId) {
     Aws::EC2::Model::Tag name_tag;
@@ -56,11 +62,53 @@ std::tuple<const Aws::String , const Aws::String>createVPC(const Aws::EC2::EC2Cl
        return std::make_tuple(EMPTY, describeVpcsOutcome.GetError().GetMessage());
    }
 
-   auto ret = nameResource(ec2, VPC_NAME_PREFIX, vpcId);
+   auto ret = nameResource(ec2, VPC_NAME, vpcId);
    if (ret != NO_ERROR) {
        return std::make_tuple(EMPTY, ret);
    }
    return std::make_tuple(vpcId, EMPTY);
+}
+
+const Aws::String dhcpOptionSets(const Aws::EC2::EC2Client &ec2, const Aws::String& vpcId) {
+
+    Aws::EC2::Model::CreateDhcpOptionsRequest createDhcpOptionsRequest;
+
+    Aws::EC2::Model::NewDhcpConfiguration dhcpConfec2Internal;
+    dhcpConfec2Internal.SetKey("domain-name");
+    Aws::Vector<Aws::String> dhcpConfec2InternalVals;
+    dhcpConfec2InternalVals.push_back(Aws::String("ec2.internal"));
+    dhcpConfec2Internal.SetValues(dhcpConfec2InternalVals);
+    createDhcpOptionsRequest.AddDhcpConfigurations(dhcpConfec2Internal);    
+
+    Aws::EC2::Model::NewDhcpConfiguration dhcpConfAmazonProvidedDNS;
+    dhcpConfAmazonProvidedDNS.SetKey("domain-name-servers");
+    Aws::Vector<Aws::String> dhcpConfAmazonProvidedDNSVals;
+    dhcpConfAmazonProvidedDNSVals.push_back(Aws::String("AmazonProvidedDNS"));
+    dhcpConfAmazonProvidedDNS.SetValues(dhcpConfAmazonProvidedDNSVals);
+    createDhcpOptionsRequest.AddDhcpConfigurations(dhcpConfAmazonProvidedDNS); 
+
+    std::cout << "creating DHCP Options... " << std::endl;
+    auto createDhcpOptionsOutcome = ec2.CreateDhcpOptions(createDhcpOptionsRequest);
+    if (!createDhcpOptionsOutcome.IsSuccess()) {
+        return createDhcpOptionsOutcome.GetError().GetMessage();
+    }
+    auto dhcpOptionsId = createDhcpOptionsOutcome.GetResult().GetDhcpOptions().GetDhcpOptionsId();
+   
+    Aws::EC2::Model::AssociateDhcpOptionsRequest associateDhcpOptionsRequest;
+    associateDhcpOptionsRequest.SetVpcId(vpcId);
+    associateDhcpOptionsRequest.SetDhcpOptionsId(dhcpOptionsId);
+    std::cout << "Associating DHCP Options... " << vpcId << std::endl;
+    auto associateDhcpOptionsOutcome = ec2.AssociateDhcpOptions(associateDhcpOptionsRequest);
+    if (!associateDhcpOptionsOutcome.IsSuccess()) {
+        return associateDhcpOptionsOutcome.GetError().GetMessage();
+    }
+
+    std::cout << "Naming DHCP id: " << dhcpOptionsId << std::endl;
+    auto ret = nameResource(ec2, "dhcp_options_onliner", dhcpOptionsId);
+    if (ret != NO_ERROR) {
+       return ret;
+    }
+    return NO_ERROR;
 }
 
 
@@ -69,15 +117,22 @@ int main(int argc, char** argv) {
     Aws::InitAPI(options);
     {
         Aws::EC2::EC2Client ec2;
-        auto result = createVPC(ec2);
-        if (std::get<1>(result) != NO_ERROR) {
+        auto vpcCreateResult = createVPC(ec2);
+        if (std::get<1>(vpcCreateResult) != NO_ERROR) {
             std::cout << "Failed to create VPC "  <<
-            std::get<1>(result) << std::endl;
+            std::get<1>(vpcCreateResult) << std::endl;
             return 1;
         }
-        const Aws::String& vpcId = std::get<0>(result);
+        const Aws::String& vpcId = std::get<0>(vpcCreateResult);
         std::cout << "Created VPC: " << vpcId << std::endl;
+
+        auto dhcpResult = dhcpOptionSets(ec2, vpcId);
+        if (dhcpResult != NO_ERROR) {
+            std::cout << "Failed to configure DHCP Sets "  << std::endl;
+            return 1;
+        }
     }    
+
     Aws::ShutdownAPI(options);
     return 0;
 }
